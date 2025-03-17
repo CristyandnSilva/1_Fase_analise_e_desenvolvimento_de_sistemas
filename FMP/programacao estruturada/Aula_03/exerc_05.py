6553dfd62cd190608274d2e0816eacc8be23a068
Cristyan das Neves Silva <cristyan.silva@aluno.fmpsc.edu.br>
2025-03-17 09:38:47 -0300

    commit

--- FMP/programacao estruturada/Aula_03/exerc_05.py
+++ FMP/programacao estruturada/Aula_03/exerc_05.py
@@ -2,5 +2,5 @@
 Fahrenheit. Faça um algoritmo para ler uma temperatura Celsius e imprimi-Ia em
 Fahrenheit (pesquise como fazer este tipo de conversão).'''
 temperatura = int(input('Digite a temperatura em Celsius para converter para Fahrenheit: '))
-Fahrenheit = (temperatura * 1.8) + 32
-print(f'A Temperatura em Celsius é {temperatura}º e em Fahrenheit é {Fahrenheit:.2f}º')
+fahrenheit = (temperatura * 1.8) + 32
+print(f'A Temperatura em Celsius é {temperatura}º e em Fahrenheit é {fahrenheit:.2f}º')
