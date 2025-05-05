93bb10f5ecd47b899415245af017ccb2280962a0
Cristyan das Neves Silva <cristyan.silva@aluno.fmpsc.edu.br>
2025-05-05 11:08:28 -0300

    att

--- Curso_em_video/Exercicios/Aula_13_for/ex50.py
+++ Curso_em_video/Exercicios/Aula_13_for/ex50.py
@@ -1,7 +1,8 @@
-'''Exercício Python 50: Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.'''    
+'''Exercício Python 50: Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares.
+ Se o valor digitado for ímpar, desconsidere-o.'''    
 soma = 0
 count = 0
-for laco in range(1,7):
+for laco in range(1,3):
     num = int(input(f'Digite o {laco}º número: '))
     if num % 2 == 0:
         soma += num
