0b77207c282b26d29cc222646261a72873b8fd4c
Cristyan das Neves Silva <cristyan.silva@aluno.fmpsc.edu.br>
2025-05-23 10:04:24 -0300

    att

--- FMP/programacao estruturada/Aula_11/exerc_03.py
+++ FMP/programacao estruturada/Aula_11/exerc_03.py
@@ -1,11 +1,12 @@
 '''3. Escreva um algoritmo que leia um valor inicial A e imprima a sequência de
 valores do cálculo de A! e o seu resultado. Ex: 5! = 5 X 4 X 3 X 2 X 1 = 120'''
-a=int(input('Digite um número inteiro positivo: '))
+fatorar=int(input('Digite um número inteiro positivo: '))
 fatorial=1
-print(f'{a}! = ', end='')
-for i in range(a, 0, -1):
+print(f'{fatorar}! = ', end='')
+for i in range(fatorar, 0, -1):
     fatorial*=i
-    if i==a:
+    if i==fatorar:
         print(i, end='')
     else:
         print(f' X {i}', end='')
+print(f' = {fatorial}')
